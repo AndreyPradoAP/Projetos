488b6992392d5e845c687d8d6fa48f5ba742a397
Andrey Prado de Oliveira <95000214+AndreyPradoAP@users.noreply.github.com>
2022-09-12 23:43:08 -0300

    att 12/09

---
+++ ProjetoPOOII_3°Bimestre(TESTE)/Centena.java
@@ -0,0 +1,11 @@
+class Centena extends Milhar{
+    private String centena;
+
+    public void setCentena(String newCentena){
+        this.centena = newCentena;
+    }
+
+    public String getCentena(){
+        return this.centena;
+    }
+}
